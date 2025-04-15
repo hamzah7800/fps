6d25614c424f4986a9d282393cdd80e49a4b2ae7
hamzah7800 <hzeb7800pass@gmail.com>
2025-04-15 15:19:04 +0100

    Update weapon.js

--- weapon.js
+++ weapon.js
@@ -5,7 +5,7 @@ class Weapon {
     }
 
     createMuzzleFlash() {
-        const flash = BABYLON.MeshBuilder.CreateSphere("flash", {diameter: 0.2}, this.camera.getScene());
+        const flash = BABYLON.MeshBuilder.CreateSphere("flash", { diameter: 0.2 }, this.camera.getScene());
         flash.material = new BABYLON.StandardMaterial("flashMat", this.camera.getScene());
         flash.material.emissiveColor = new BABYLON.Color3(1, 0.8, 0.3);
         flash.parent = this.camera;
@@ -18,12 +18,23 @@ class Weapon {
         this.muzzleFlash.isVisible = true;
         setTimeout(() => this.muzzleFlash.isVisible = false, 100);
 
-        const ray = this.camera.getForwardRay(500);
+        const ray = this.camera.getForwardRay(500); // Extended range
         const hit = this.camera.getScene().pickWithRay(ray);
 
-        if (hit.pickedMesh && hit.pickedMesh.metadata?.reference) {
-            hit.pickedMesh.metadata.reference.takeDamage(20);
-            console.log("Hit enemy! Remaining health:", hit.pickedMesh.metadata.health);
+        if (hit.pickedMesh) {
+            console.log("Ray hit:", hit.pickedMesh.name, hit.pickedPoint);
+
+            if (hit.pickedMesh.metadata?.reference) {
+                hit.pickedMesh.metadata.reference.takeDamage(20);
+                console.log("Enemy hit! Remaining health:", hit.pickedMesh.metadata.health);
+            }
+        } else {
+            console.log("Ray didn't hit anything.");
         }
+    }
+
+    reload() {
+        // Optional: Implement reload logic if needed
+        console.log("Reloading...");
     }
 }
